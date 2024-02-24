b7090d461769b8fde6ff038dcaa0de3ebee4fd8f
Ritesh001 <160412969+riteshtmglama001@users.noreply.github.com>
2024-02-24 19:01:31 +0545

    ichange

--- connection/info.php
+++ connection/info.php
@@ -107,7 +107,7 @@ body{
                 </tbody>
               </table>
               <center>
-              <a href="add.php"><button id="hero" style="cursor:pointer; padding:10px;
+              <a href="edit.php"><button id="hero" style="cursor:pointer; padding:10px;
               width:80px;border-radius:150px;font-size:20px; margin-top:60px;background-color:orangered;color:white;
               ">Add</button></a>
               </center>
